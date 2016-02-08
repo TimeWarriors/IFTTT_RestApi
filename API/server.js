4e12c0e56d43f15a633ee1d7a2ca4285714e9d3e
Lucas <lucas.94wik@gmail.com>
2016-02-08 11:55:43 +0100

    Set up so we can do localhost calls.

--- API/server.js
+++ API/server.js
@@ -2,6 +2,19 @@
 
 //../settings/settings.json
 
-
+var express = require('express');
 var app = express();
+//var bodyParser = require('body-parser');
 
+//app.use(bodyParser.json());
+
+//var port = process.env.PORT || 8080;
+
+
+app.get('/', function(req, res){
+	res.send("<!doctype html><html><head><meta charset='utf-8' /><title>Blink RESTApi</title></head><body>Hello world</body></html>");
+})
+
+
+app.listen(3000);
+//console.log("You're on port " + port)
