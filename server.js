fe5a0a425c0281a2c55339494ae2746e65c28e23
Lucas <lucas.94wik@gmail.com>
2016-02-08 13:25:20 +0100

    API is finished. NOT TESTED THO. Since we dont know how they want it hosted atm there might be some changes to how it's called. This will be specified tomorrow after the client meeting.

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var fsp = require("fs-promise");
+
+//Filler page
+app.get('/', function(req, res){
+	res.send("<!doctype html><html><head><meta charset='utf-8' /><title>Blink RESTApi</title></head><body>Lorem Ipsum</body></html>");
+});
+
+//Sets the presence in the settings JSON file to the users current presence.
+app.post('/', function(req, res){
+	
+	var data = JSON.parse(req.body);
+	
+	fsp.readFile('../settings/settings.json', {encoding:'utf8'}).then((contents) =>{
+		var parsedContent = JSON.parse(contents);		
+		
+		for (var i = 0; i < parsedContent.length; i++){
+			
+			if(parsedContent[i].type === "blink" && parsedContent[i].userID === data.id){
+				parsedContent[i].presence = data.presence;
+				
+				fsp.writeFile('../settings/settings.json', JSON.stringify(parsedContent)).then(() => {
+					console.log("overwritten");
+				});
+			}
+		}
+	});
+});
+
+
+app.listen(3000);
