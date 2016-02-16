bf397bbb9e2b15215f5b5ad75229cdb405a92a9b
Lucas <lucas.94wik@gmail.com>
2016-02-16 17:21:19 +0100

    Started to work with promises and next thing to do is to initiate the event when a user enters busy mode.

--- scheduleHandler.js
+++ scheduleHandler.js
@@ -1,11 +1,12 @@
 var fsp = require("fs-promise");
+var http = require('http');
+var Promise = require('promise')
 var nodeSchedule = require('node-schedule');
 var timeEditApi = require('timeeditapi');
 var timeEdit = new timeEditApi('https://se.timeedit.net/web/lnu/db1/schema1/', 3);
 
 function scheduleHandler(){
-	this.InitiateTimer();
-	
+	this.InitiateTimer();	
 }
 
 scheduleHandler.prototype.InitiateTimer = function(){
@@ -13,27 +14,64 @@ scheduleHandler.prototype.InitiateTimer = function(){
 	//rule.hour = 5;
 	rule.second = 30;
 	
-	nodeSchedule.scheduleJob(rule, this.testRun);
+	var that = this;
 	
-	this.testRun();
+	nodeSchedule.scheduleJob(rule, function(){
+		that.InitiateEvents().then((data)=>{
+			that.CreateEvents(data);
+		});		
+	});
+	
+	
 }
 
 scheduleHandler.prototype.getTodaysSchedule = function(){
 	timeEdit.getTodaysSchedule('Johan Leitet').then((schedule) =>{
 		console.log(JSON.stringify(schedule, null, 2));
-		console.log(schedule[0].booking.time.startTime)
+		console.log(schedule[0].booking.time.startTime);
 	}).catch((err)=>{
 		console.log("ERROR: "+ err);
 	})
 }
 
-scheduleHandler.prototype.testRun = function(){
-	timeEdit.search('John_Häggerud')
+//Returns a promise that gives the public data from the usersetting.json file
+scheduleHandler.prototype.InitiateEvents = function(){
+	return new Promise((resolve, reject) =>{
+		var options = {
+			host: "localhost",
+			path: "/userData",
+			method: 'GET',
+			port: '3000'
+		
+		};
+
+		http.get(options, function(res){
+
+			var chunks = [];
+			res.on('data', (chunk)=>{			
+				chunks.push(chunk);	
+
+			}).on('end', ()=>{			
+				return resolve(JSON.parse(Buffer.concat(chunks)));				
+			})
+		}).on('error',(err) =>{
+			reject(err);
+		});	
+   });
+}
+
+scheduleHandler.prototype.CreateEvents = function(users){
+	console.log(users);
+	
+}
+
+scheduleHandler.prototype.StartEvent = function(data){
+	/*timeEdit.search('John_Häggerud')
     .then((result) => {
         console.log(JSON.stringify(result, null ,2));
     }).catch((er) => {
         console.log(er);
-    });
+    });*/
 }
 
 module.exports = scheduleHandler;
